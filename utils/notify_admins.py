bd028bf1c0013b617d6a9f1f8bf5a58cd238b37e
Botirov Boburbek <74315658+Boburshoh-oss@users.noreply.github.com>
2022-03-11 22:22:28 +0500

    Update notify_admins.py
    switch off notify admin

--- utils/notify_admins.py
+++ utils/notify_admins.py
@@ -5,10 +5,10 @@ from aiogram import Dispatcher
 from data.config import ADMINS
 
 
-async def on_startup_notify(dp: Dispatcher):
-    for admin in ADMINS:
-        try:
-            await dp.bot.send_message(admin, "Bot ishga tushdi")
+# async def on_startup_notify(dp: Dispatcher):
+#     for admin in ADMINS:
+#         try:
+#             await dp.bot.send_message(admin, "Bot ishga tushdi")
 
-        except Exception as err:
-            logging.exception(err)
+#         except Exception as err:
+#             logging.exception(err)
